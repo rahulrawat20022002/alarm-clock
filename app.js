50601b4e8c875d61939b96f6ff3ed5b5a2442e30
rahulrawat20022002 <94779184+rahulrawat20022002@users.noreply.github.com>
2023-09-27 01:56:50 +0530

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+const time=document.getElementById('time')
+
+function getTime(){
+
+    setInterval(()=>{
+        // console.log("testing")
+        // time.innerHTML=hr+":"+min+":"+sec
+        console.log("testing");
+        time.innerHTML=
+    },1000)
+}
+getTime()
