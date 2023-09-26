d12fa022c555eca07bb936cc9f44611707cfafbe
rahulrawat20022002 <94779184+rahulrawat20022002@users.noreply.github.com>
2023-09-27 02:29:34 +0530

    update

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const time=document.getElementById('time')
-
 function getTime(){
-
+    
     setInterval(()=>{
-        // console.log("testing")
-        // time.innerHTML=hr+":"+min+":"+sec
-        console.log("testing");
-        time.innerHTML=
+        let date=new Date();
+        time.innerHTML=date.toLocaleTimeString()
+        // console.log(date.toLocaleTimeString());
     },1000)
 }
 getTime()
+const alarmbtn=document.getElementById('alarmbtn');
+const inputfield=document.getElementById('inputfield')
+alarmbtn.addEventListener('click',function(){
+    console.log("success");
+    inputfield.style.display='inline';
+
+})
+const btn=document.querySelector('button')
+const form=document.querySelector('form')
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    console.log(e.target.hours.value)
+    console.log(e.target.minutes.value)
+    console.log(e.target.seconds.value)
+})
